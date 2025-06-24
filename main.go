aba47af9d851fcb0b1a69fc7e935dd66437f493d
mochie~! <187453775+mochienya@users.noreply.github.com>
2025-06-24 06:21:31 +0200

    update deps, move to x/crypto/openpgp, add handling for keys with passwords

--- main.go
+++ main.go
@@ -2,37 +2,56 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
 	"os"
+	"strings"
 	"time"
+
 	"regexp"
 
-	"github.com/ProtonMail/gopenpgp/v3/crypto"
 	clipboard "github.com/tiagomelo/go-clipboard/clipboard"
+	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/openpgp/armor"
 )
 
 func main() {
-	keyFile, err := os.Open("private_key.asc")
+	keyReader, err := os.Open("private_key.asc")
 	if err != nil { errHandle("Failed to read private_key.asc, does it exist?") }
-	defer keyFile.Close()
+	defer keyReader.Close()
+
+	keyring, err := openpgp.ReadArmoredKeyRing(keyReader)
+	if err != nil { errHandle("Invalid key in priv.asc") }
+
+	if keyring[0].PrivateKey.Encrypted {
+		for {
+			fmt.Print("Input password for private key:")
+			var input string
+			fmt.Scanln(input)
+			if err := keyring[0].PrivateKey.Decrypt([]byte(input)); err == nil {
+				fmt.Println("")
+				break
+			}
+			fmt.Println("\033[31mIncorrect password\033[0m")
+		}
+	}
 
 	clip := clipboard.New()
-	pgp := crypto.PGP()
-
 	clipContents, err := clip.PasteText()
-	if err != nil { log.Panic("Failed to read clipboard") }
+	if err != nil { errHandle("Failed to read clipboard") }
 
-	privKey, err := crypto.NewKeyFromReader(keyFile)
-	if err != nil { errHandle("Invalid key in file private_key.asc") }
-	decHandler, err := pgp.Decryption().DecryptionKey(privKey).New()
-	if err != nil { errHandle("yeah this wasn't my fault (i think). something went really fucking wrong") }
-	decrypted, err := decHandler.Decrypt([]byte(clipContents), crypto.Auto)
-	if err != nil { errHandle("Decryption failed, is the message in your clipboard correct?") }
+	armoredMessage, err := armor.Decode(strings.NewReader(clipContents))
+	if err != nil { errHandle(err.Error()) }
+
+	messageReader, err := openpgp.ReadMessage(armoredMessage.Body, keyring, nil, nil)
+	if err != nil { errHandle(err.Error()) }
+
+	message, _ := io.ReadAll(messageReader.UnverifiedBody)
 
 	regex := regexp.MustCompile(`[0-9a-f]{56}`)
-	final := regex.FindString(string(decrypted.Bytes()[:]))
+	final := regex.Find(message)
+	if final == nil { errHandle("Message did not contain an abacus verification key") }
 
-	clip.CopyText(final)
+	clip.CopyText(string(final))
 	fmt.Println("\033[32mKey copied to clipboard!\033[0m")
 	time.Sleep(2 * time.Second)
 	os.Exit(0)
